c8179660a9f464c3c0e3e360e8f0515aa5d7c4cd
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2017-08-30 20:33:31 -0700

    First Commit

---
+++ app/src/main/java/com/example/lyber/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lyber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btCompare;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btCompare = (Button) findViewById(R.id.buttonCompaer);
+
+
+        btCompare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+
+
+
+}
