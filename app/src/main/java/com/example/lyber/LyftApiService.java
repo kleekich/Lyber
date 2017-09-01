6477de870169c5d316eff3863552a7efa2c56a9d
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2017-08-31 18:18:12 -0700

    LyftApiService implemented

--- app/src/main/java/com/example/lyber/LyftApiService.java
+++ app/src/main/java/com/example/lyber/LyftApiService.java
@@ -16,51 +16,48 @@ public class LyftApiService {
     private Call<LyftModel> mCall;
 
     public LyftApiService(RetrofitClient retrofitClient){
-        uberApi = retrofitClient.create(UberApiService.UberApi.class);
+        lyftApi = retrofitClient.create(LyftApiService.LyftService.class);
     }
 
-    public void getUber(final ResponseHandler<UberModel> callBack,
-                        boolean isCancelable){
-        Call<UberModel> call = uberApi.getEstimatePrice();
-        if(isCancelable){
+    public void getLyft(final ResponseHandler<LyftModel> callBack,
+                        boolean isCancelable) {
+        Call<LyftModel> call = lyftApi.getEstimatePrice();
+        if (isCancelable) {
             //if data is present return;
-            if(mCall != null){
+            if (mCall != null) {
                 return;
-            } else{
+            } else {
                 mCall = call;
             }
         }
 
-        call.enqueue(new Callback<UberModel>() {
+        call.enqueue(new Callback<LyftModel>() {
             @Override
-            public void onResponse(Call<UberModel> call, Response<UberModel> response) {
+            public void onResponse(Call<LyftModel> call, Response<LyftModel> response) {
                 mCall = null;
-                if( response.isSuccessful()){
+                if (response.isSuccessful()) {
                     callBack.onSuccess(response.body());
-                }else{
+                } else {
                     callBack.onError(response.code(), response.errorBody().toString());
                 }
             }
 
             @Override
-            public void onFailure(Call<UberModel> call, Throwable t) {
-                mCall= null;
+            public void onFailure(Call<LyftModel> call, Throwable t) {
+                mCall = null;
                 callBack.onError(-1, t.getMessage());
 
             }
         });
-
+    }
 
 
     public interface LyftService {
         @Headers({"Content-Type: application/json",
                 "Authorization: Bearer VCz8t0iW4Xq60kzG2bk9A70irK+bJMQxpVwoEvMrsWtpR0z3GfIoD5Yxw0ih39AQSkRiiYbnyj7yQFXTqsGxsLf3nhHIw3KzTZrIS31iZ+CvFovYwicNmFo="
-
         })
         @GET("https://api.lyft.com/v1/eta?lat=37.7833&lng=-122.4167")
         Call<LyftModel> getEstimatePrice();
-
-
 
     }
 
